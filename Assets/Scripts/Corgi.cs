afd1dd0545853d3ff4bd34d39f5288e29dc15c5d
PilouPilouPilou <pierrelouis.paget@gmail.com>
2026-03-17 15:43:15 -0700

    Added drunkenness

--- Assets/Scripts/Corgi.cs
+++ Assets/Scripts/Corgi.cs
@@ -1,9 +1,16 @@
+using System;
+using System.Collections;
+using System.Threading;
 using UnityEngine;
 
 public class Corgi : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
-
+    private bool isDrunk = false;
+    public Sprite DrunkSprite;
+    public Sprite SoberSprite;
+    private Coroutine soberUpCoroutine;
+    
     public void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -11,6 +18,8 @@ public class Corgi : MonoBehaviour
     
     public void Move(Vector2 direction)
     {
+        direction = ApplyDrunkenness(direction);
+        
         FaceCorrectDirection(direction);
         
         Vector2 movementAmount = direction * (GameParameters.CorgiMoveSpeed * Time.deltaTime);
@@ -18,6 +27,74 @@ public class Corgi : MonoBehaviour
         spriteRenderer.transform.Translate(movementAmount.x,movementAmount.y,0);
 
         spriteRenderer.transform.position = SpriteTools.ConstrainToScreen(spriteRenderer);
+    }
+
+    private Vector2 ApplyDrunkenness(Vector2 direction)
+    {
+        if (isDrunk)
+        {
+            direction.x = direction.x * -1;
+            direction.y = direction.y * -1;
+        }
+        return direction;
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        print(message: "boom");
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Beer")
+        {
+            GetDrunk();
+            Destroy(other.gameObject);
+        }
+        if (other.tag == "Bone")
+        {
+            print(message: "Do bone things");
+        }
+        if (other.tag == "Pill")
+        {
+            print(message: "Do pill things");
+        }
+    }
+
+    private void GetDrunk()
+    {
+        isDrunk = true;
+        ChangeToDrunkSprite();
+        StartSoberingUp();
+    }
+
+    private void StartSoberingUp()
+    {
+        if (soberUpCoroutine != null)
+            StopCoroutine(soberUpCoroutine);
+        soberUpCoroutine = StartCoroutine(CountdownUntilSober());
+    }
+
+    IEnumerator CountdownUntilSober()
+    {
+        yield return new WaitForSeconds(GameParameters.CorgiDrunkSeconds);
+        SoberUp();
+    }
+
+    private void SoberUp()
+    {
+        ChangeToSoberSprite();
+        isDrunk = false;
+    }
+
+    private void ChangeToSoberSprite()
+    {
+        spriteRenderer.sprite = SoberSprite;
+    }
+
+    private void ChangeToDrunkSprite()
+    {
+        spriteRenderer.sprite = DrunkSprite;
     }
 
     private void FaceCorrectDirection(Vector2 direction)
--- Assets/Scripts/GameParameters.cs
+++ Assets/Scripts/GameParameters.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public static class GameParameters
 {
     public static float CorgiMoveSpeed = 5f;
+    public static float CorgiDrunkSeconds = 5f;
+
     
     public static float PoopSecondsOnScreen = 4f;
     
